9692957dbf1f3d5b4a1d85ff2dae4461276096c3
amalhanaja <amalhanaja@gmail.com>
2017-06-20 22:56:12 +0700

    m

--- app/jadwal.py
+++ app/jadwal.py
@@ -8,16 +8,16 @@ from datetime import datetime
 def jadwal():
 
 	class Zone(tzinfo):
-    def __init__(self,offset,isdst,name):
-        self.offset = offset
-        self.isdst = isdst
-        self.name = name
-    def utcoffset(self, dt):
-        return timedelta(hours=self.offset) + self.dst(dt)
-    def dst(self, dt):
-            return timedelta(hours=1) if self.isdst else timedelta(0)
-    def tzname(self,dt):
-         return self.name
+	    def __init__(self,offset,isdst,name):
+	        self.offset = offset
+	        self.isdst = isdst
+	        self.name = name
+	    def utcoffset(self, dt):
+	        return timedelta(hours=self.offset) + self.dst(dt)
+	    def dst(self, dt):
+	            return timedelta(hours=1) if self.isdst else timedelta(0)
+	    def tzname(self,dt):
+	         return self.name
          
     GMT = Zone(7,False,'GMT')
 	sekarang = datetime.now(GMT).strftime('%Y-%m-%d %H:%M:%S')
